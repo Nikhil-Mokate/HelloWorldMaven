b0bb6bb67d88fc20544b008d87e12edf11f80f45
Nikhil Mokate <nikhilshelldb@gmail.com>
2022-09-23 13:21:15 +0530

    added maven project

---
+++ src/main/java/com/vodafone/app/App.java
@@ -0,0 +1,13 @@
+package com.vodafone.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Amazing World Again!" );
+    }
+}
